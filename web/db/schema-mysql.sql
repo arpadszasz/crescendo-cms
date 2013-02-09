f58f5ec5498e115829a0c61acb4dab44c13e75d7
Árpád Szász <arpad.szasz@plenum.ro>
2013-02-09 08:32:57 +0200

    MySQL database schema

---
+++ web/db/schema-mysql.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS crescendo_cms;
+CREATE DATABASE IF NOT EXISTS crescendo_cms CHARSET=utf8;
+USE crescendo_cms;
+SET NAMES utf8;
+
+DROP TABLE IF EXISTS `csnd_user`;
+CREATE TABLE `categories` (
+    `id`                INT NOT NULL AUTO_INCREMENT,
+    `username`          VARCHAR(64) NOT NULL,
+    `password`          VARCHAR(128) NOT NULL,
+    `email`             VARCHAR(128) NOT NULL,
+    `name`              TEXT,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8;
